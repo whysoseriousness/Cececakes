be439480c719ef7893d0b6227d1288f098501675
Cecile Schilis-Gallego <csgallego@gmail.com>
2014-07-25 22:18:13 -0400

    buttons menu redone

---
+++ tandem/ChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.h
+//  tandem
+//
+//  Created by Cécile Schilis-Gallego on 7/25/14.
+//  Copyright (c) 2014 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatViewController : UIViewController
+
+@end
