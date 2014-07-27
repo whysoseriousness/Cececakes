f53077d934f57845be4fe30ff38abe6cc2e0b88d
Cecile Schilis-Gallego <csgallego@gmail.com>
2014-07-26 22:00:59 -0400

    ceceTableViewcontroller

---
+++ ceceTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ceceTableViewController.h
+//  tandem
+//
+//  Created by Cécile Schilis-Gallego on 7/26/14.
+//  Copyright (c) 2014 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface ceceTableViewController : UITableViewController
+@property NSMutableArray * bikeTableData;
+@end
